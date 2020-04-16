8cf01f8f5913e062ec283e1e4650bdf5daa77fba
myfrank4everreal <myfrank4everrel@gmail.com>
2020-04-16 09:10:18 +0400

    general maintenance

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.agencyHome, name='home'),
+    path('about', views.aboutUs, name='about'),
+    path('contact', views.contactUs, name='contact'),
+    path('services', views.services, name='services'),
 #     path('demoaboutus', views.demoabout, name='demoabout'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,52 @@
 from django.shortcuts import render
 from .models import modeltemp
+from django.core.mail import send_mail
 
 def agencyHome(request):
     context = {} 
     return render(request, 'myapp/agencyhome.html', context)
 
+
+def aboutUs(request):
+    context = {} 
+    return render(request, 'myapp/aboutus.html', context)
+
+
+def contactUs(request):
+    if request.method == "POST":
+        message_phone = request.POST['phone']
+        message_name  = request.POST['name']
+        message_email  = request.POST['email']
+        message = request.POST['message']
+        
+        
+
+
+
+        send_mail(
+            message,  
+            message_phone,      
+            message_email, 
+            message_name,
+
+            
+            
+            ['franklin.okolie4@gmail.com']
+            )
+            
+
+       
+
+
+
+        return render(request, 'myapp/contactus.html', {'message_name':message_name})
+    else:
+        return render(request, 'myapp/contactus.html', {})
+
+
+def services(request):
+    context = {} 
+    return render(request, 'myapp/services.html', context)
 
 
 # this is for the about page
